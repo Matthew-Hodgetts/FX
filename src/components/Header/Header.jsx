152ac9dec51024cb56bbb4483f4d73d6c03ed372
Matthew Hodgetts <mpolar999@gmail.com>
2020-08-24 14:30:09 +0100

    Base setup done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import styles from "./Header.module.scss"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Forex pro</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout/Layout"
+import SEO from "../components/Seo/Seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+  </Layout>
+)
+
+export default IndexPage
